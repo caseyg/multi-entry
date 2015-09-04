86e1e042d8f9caf6d7c359e6d63ff4df483d9943
CX <hey@christinaxu.org>
2015-09-04 16:56:08 -0400

    Style changes

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -3,7 +3,6 @@
   <?php foreach(page('home')->children()->visible() as $project): ?>
   <li>
     <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
-    <p class="hosted"><?php echo $project->sitename()->html() ?></p>
     <a href="<?php echo $project->link() ?>">
       <img src="<?php echo $image->url() ?>" alt="<?php echo $project->title()->html() ?>" >
     </a>
