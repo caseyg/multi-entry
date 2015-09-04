b68837201358ff6b4efd3c492b68e9bfef1ddd5b
Casey Gollan <hello@caseyagollan.com>
2015-09-04 15:44:58 -0400

    add tags as grid item class name, fixes filtering!

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -1,7 +1,12 @@
 
 <ul class="grid cf">
   <?php foreach(page('home')->children()->visible() as $project): ?>
-  <li>
+  <?php 
+      $tags=($project->tags());
+      $tags=explode(',',$tags);
+      $tags=array_map('trim',$tags);
+  ?> 
+  <li class="<?php foreach($tags as $tag): ?><?php echo html($tag) ?> <?php endforeach ?>">
     <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
     <p class="hosted"><?php echo $project->sitename()->html() ?></p>
     <a href="<?php echo $project->link() ?>">
