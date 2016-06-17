4b100881f13f2e1b1047c83507319f5080b7d80c
CX <hey@christinaxu.org>
2016-06-17 14:10:29 -0400

    Changed menu

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -5,7 +5,7 @@
     <li>
       <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
 
-      <?php if($p->hasVisibleChildren()): ?>
+    <?php if($p->hasVisibleChildren()): ?>
       <ul class="submenu">
         <?php foreach($p->children()->visible() as $p): ?>
         <li>
@@ -17,13 +17,13 @@
 
     </li>
     <?php endforeach ?>
-    <li><a href="/#filter=*" data-filter="*">all</a></li>
-    <li><a href="/#filter=.games" data-filter=".games">games</a></li>
+    <li><a href="/#filter=*" data-filter="*">online</a></li>
+    <li><a href="http://slangkas.multientry.com">slang cards</a></li>
+    <!--<li><a href="/#filter=.games" data-filter=".games">games</a></li>
     <li><a href="/#filter=.fashion" data-filter=".fashion">fashion</a></li>
     <li><a href="/#filter=.music" data-filter=".music">music</a></li>
     <li><a href="/#filter=.tech" data-filter=".tech">tech</a></li>
     <li><a href="/#filter=.internets" data-filter=".internets">internets</a></li>
-    <li><a href="/#filter=.art" data-filter=".internets">art</a></li>
+    <li><a href="/#filter=.art" data-filter=".internets">art</a></li>-->
   </ul>
-
 </nav>
