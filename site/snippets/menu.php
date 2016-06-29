52d603a6c201fa37c035c74e43ddc9495c5d8deb
CX <hey@christinaxu.org>
2016-06-29 16:34:48 -0400

    Changed menu

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,6 +1,18 @@
 <nav role="navigation">
 
   <ul class="menu cf button-group filter-button-group">
+    <li><a href="/#filter=*" data-filter="*">all</a></li>
+    <li><a href="/#filter=.writing" data-filter=".writing">writing</a></li>
+    <li><a href="/#filter=.products" data-filter=".products">store</a></li>
+    <!--<li><a href="/#filter=.season1" data-filter=".season1">season 1</a></li>
+    <li><a href="/#filter=.games" data-filter=".games">games</a></li>
+    <li><a href="/#filter=.fashion" data-filter=".fashion">fashion</a></li>
+    <li><a href="/#filter=.music" data-filter=".music">music</a></li>
+    <li><a href="/#filter=.tech" data-filter=".tech">tech</a></li>
+    <li><a href="/#filter=.internets" data-filter=".internets">internets</a></li>
+    <li><a href="/#filter=.art" data-filter=".internets">art</a></li>-->
+  </ul>
+  <ul class="menu right-align">
     <?php foreach($pages->visible() as $p): ?>
     <li>
       <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
@@ -17,13 +29,5 @@
 
     </li>
     <?php endforeach ?>
-    <li><a href="/#filter=*" data-filter="*">online</a></li>
-    <li><a href="http://slangkas.multientry.com">slang cards</a></li>
-    <!--<li><a href="/#filter=.games" data-filter=".games">games</a></li>
-    <li><a href="/#filter=.fashion" data-filter=".fashion">fashion</a></li>
-    <li><a href="/#filter=.music" data-filter=".music">music</a></li>
-    <li><a href="/#filter=.tech" data-filter=".tech">tech</a></li>
-    <li><a href="/#filter=.internets" data-filter=".internets">internets</a></li>
-    <li><a href="/#filter=.art" data-filter=".internets">art</a></li>-->
   </ul>
 </nav>
